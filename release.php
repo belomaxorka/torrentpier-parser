13d81c1b81b1709c2284aeda9a6b8e34393f36ad
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-29 16:34:50 +0700

    Updated

--- release.php
+++ release.php
@@ -220,6 +220,197 @@ function rgb2html($r, $g = -1, $b = -1)
 	return '#' . $color;
 }
 
+/**
+ * ------------------------------------------------------
+ * Список доступных трекеров
+ * ------------------------------------------------------
+ */
+$trackers = array(
+	'rutracker' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "/https:\/\/rutracker.org\/forum\/viewtopic.php\?t=/",
+		'login_url' => 'https://rutracker.org/forum/login.php',
+		'dl_url' => 'https://rutracker.org/forum/dl.php?t=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<div style="padding-top: 6px;">'
+	),
+	'rutor' => array(
+		'enabled' => true,
+		'regex' => "#(?:rutor\.info|rutor\.is)\/torrent/#", // .is, .info
+		'dl_url' => 'http://d.rutor.info/download/',
+		'target_element' => '<td class="header"',
+		'redirect' => array(
+			'from' => array('http://rutor.org/'),
+			'to' => 'http://rutor.info/'
+		)
+	),
+	'nnmclub' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#https://nnmclub.to/forum/viewtopic.php\?t=#",
+		'login_url' => 'https://nnmclub.to/forum/login.php',
+		'dl_url' => 'https://nnmclub.to/forum/download.php?id=',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<span class="seedmed">'
+	),
+	'rustorka' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#http://rustorka.com/forum/viewtopic.php\?t=#",
+		'login_url' => 'http://rustorka.com/forum/login.php',
+		'dl_url' => 'http://rustorka.com/forum/download.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<tr class="row3 tCenter">'
+	),
+	'booktracker' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "/https:\/\/booktracker.org\/viewtopic.php\?t=/",
+		'login_url' => 'https://booktracker.org/login.php',
+		'dl_url' => 'https://booktracker.org/download.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<div id="tor_info"'
+	),
+	'torrentwindows' => array(
+		'enabled' => true,
+		'regex' => "#torrent-wind.net/#",
+		'dl_url' => 'https://torrent-wind.net/index.php?do=download&id=',
+		'target_element' => '<div class="fdl-btn-size fx-col fx-center">'
+	),
+	'riperam' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#riperam.org/#",
+		'login_url' => 'http://riperam.org/ucp.php?mode=login',
+		'dl_url' => 'http://riperam.org/download/file.php?id=',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<td style="text-align: center; vertical-align: top;">'
+	),
+	'mptor' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#(?:megapeer\.ru|megapeer\.vip)\/torrent/#", // .ru, .vip
+		'login_url' => 'http://megapeer.ru/takelogin.php',
+		'dl_url' => 'http://megapeer.ru/download/',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<td class="heading"',
+		'redirect' => array(
+			'from' => array('http://megapeer.vip/'),
+			'to' => 'http://megapeer.ru/'
+		)
+	),
+	'tapochek' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "/https:\/\/tapochek.net\/viewtopic.php\?t=/",
+		'login_url' => 'https://tapochek.net/login.php',
+		'dl_url' => 'https://tapochek.net/download.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<p><img src="images/icon_dn.png"',
+	),
+	'uniongang' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#uniongang.club/torrent-#",
+		'login_url' => 'http://uniongang.club/takelogin.php',
+		'dl_url' => 'http://uniongang.club/download.php?id=',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<form method="post" action="takerate.php">',
+	),
+	'kinozal' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#kinozal.tv/details.php\?id=#",
+		'login_url' => 'http://kinozal.tv/takelogin.php',
+		'dl_url' => 'http://dl.kinozal.tv/download.php?id=',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<form id="cmt" method=post',
+	),
+	'kinozalguru' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#kinozal.guru/details.php\?id=#",
+		'login_url' => 'https://kinozal.guru/takelogin.php',
+		'dl_url' => 'http://dl.kinozal.guru/download.php?id=',
+		'login_input_name' => 'username',
+		'password_input_name' => 'password',
+		'target_element' => '<form id="cmt" method=post',
+	),
+	'windowssoftinfo' => array(
+		'enabled' => true,
+		'regex' => "#windows-soft.info/#",
+		'dl_url' => 'https://windows-soft.info/engine/download.php?id=',
+		'target_element' => '<div class="fstory-rating">'
+	),
+	'ztorrents' => array(
+		'enabled' => true,
+		'regex' => "#z-torrents.ru/#",
+		'dl_url' => '', // TODO
+		'target_element' => '<div class="dle_b_appp"'
+	),
+	'piratbit' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#piratbit.org/topic/#",
+		'login_url' => 'https://piratbit.org/login.php',
+		'dl_url' => 'https://piratbit.org/dl.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<span class="fs11_bold thanked">',
+	),
+	'onlysoft' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "#https://only-soft.org/viewtopic.php\?t=#",
+		'login_url' => 'https://only-soft.org/login.php',
+		'dl_url' => 'https://only-soft.org/download.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<p class="small">',
+	),
+	'rutrackerru' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "/http:\/\/rutracker.ru\/viewtopic.php\?t=/",
+		'login_url' => 'http://rutracker.ru/login.php',
+		'dl_url' => 'http://rutracker.ru/dl.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<input type="radio" name=',
+	),
+	'ddgroupclub' => array(
+		'enabled' => true,
+		'auth' => true,
+		'regex' => "/http:\/\/ddgroupclub.win\/viewtopic.php\?t=/",
+		'login_url' => 'http://ddgroupclub.win/login.php',
+		'dl_url' => 'http://ddgroupclub.win/dl.php?id=',
+		'login_input_name' => 'login_username',
+		'password_input_name' => 'login_password',
+		'target_element' => '<p class="small">',
+	),
+	'xxxtor' => array(
+		'enabled' => true,
+		'regex' => "#xxxtor.net#",
+		'dl_url' => 'https://', // TODO
+		'target_element' => '<div id="down">',
+	)
+);
+
+/**
+ * ------------------------------------------------------
+ * Проверка авторизации
+ * ------------------------------------------------------
+ */
 if ($bb_cfg['torrent_parser']['parser_auth'] === 'user' && (IS_GROUP_MEMBER && !empty($bb_cfg['torrent_parser']['group_id']))) {
 	// Проверка на участника группы
 	$groups = implode(',', $bb_cfg['torrent_parser']['group_id']);
@@ -239,6 +430,9 @@ if ($bb_cfg['torrent_parser']['parser_auth'] === 'user' && (IS_GROUP_MEMBER && !
 	}
 }
 
+/**
+ * Формирование главной страницы
+ */
 if (empty($url)) {
 	// Получаем все форумы
 	if (!$forums = $datastore->get('cat_forums')) {
@@ -292,8 +486,8 @@ if (empty($url)) {
 	$cat_forum_select = "\n<select class=\"form-control form-control-sm\" id=\"fs\" name=\"forum_id\" style=\"font-size: small;\">\n" . $search_all_opt . $opt . "</select>\n";
 
 	$template->assign_vars(array(
-		'URL' => true,
-		'URL_DISPLAY' => 'tapochek.net, rutor.info / rutor.is, booktracker.org, megapeer.ru / megapeer.vip, riperam.org, torrent-wind.net, only-soft.org, z-torrents.ru, uniongang.club, <br> rustorka.com, nnmclub.to, rutracker.org, rutracker.ru, kinozal.tv / kinozal.guru, piratbit.org,  ddgroupclub.win, xxxtor.net',
+		'IN_PARSER' => true,
+		'SUPPORTED_TRACKERS' => implode(', ', array_keys($trackers)),
 		'SELECT_FORUM' => $cat_forum_select,
 	));
 } else {
@@ -309,188 +503,6 @@ if (empty($url)) {
 	// Настройка прокси
 	// $curl->setProxy(''); // ip:port
 	// $curl->setProxyAuth('login:pass'); // login:pass
-
-	// Список всех трекеров
-	$trackers = array(
-		'rutracker' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "/https:\/\/rutracker.org\/forum\/viewtopic.php\?t=/",
-			'login_url' => 'https://rutracker.org/forum/login.php',
-			'dl_url' => 'https://rutracker.org/forum/dl.php?t=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<div style="padding-top: 6px;">'
-		),
-		'rutor' => array(
-			'enabled' => true,
-			'regex' => "#(?:rutor\.info|rutor\.is)\/torrent/#", // .is, .info
-			'dl_url' => 'http://d.rutor.info/download/',
-			'target_element' => '<td class="header"',
-			'redirect' => array(
-				'from' => array('http://rutor.org/'),
-				'to' => 'http://rutor.info/'
-			)
-		),
-		'nnmclub' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#https://nnmclub.to/forum/viewtopic.php\?t=#",
-			'login_url' => 'https://nnmclub.to/forum/login.php',
-			'dl_url' => 'https://nnmclub.to/forum/download.php?id=',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<span class="seedmed">'
-		),
-		'rustorka' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#http://rustorka.com/forum/viewtopic.php\?t=#",
-			'login_url' => 'http://rustorka.com/forum/login.php',
-			'dl_url' => 'http://rustorka.com/forum/download.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<tr class="row3 tCenter">'
-		),
-		'booktracker' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "/https:\/\/booktracker.org\/viewtopic.php\?t=/",
-			'login_url' => 'https://booktracker.org/login.php',
-			'dl_url' => 'https://booktracker.org/download.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<div id="tor_info"'
-		),
-		'torrentwindows' => array(
-			'enabled' => true,
-			'regex' => "#torrent-wind.net/#",
-			'dl_url' => 'https://torrent-wind.net/index.php?do=download&id=',
-			'target_element' => '<div class="fdl-btn-size fx-col fx-center">'
-		),
-		'riperam' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#riperam.org/#",
-			'login_url' => 'http://riperam.org/ucp.php?mode=login',
-			'dl_url' => 'http://riperam.org/download/file.php?id=',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<td style="text-align: center; vertical-align: top;">'
-		),
-		'mptor' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#(?:megapeer\.ru|megapeer\.vip)\/torrent/#", // .ru, .vip
-			'login_url' => 'http://megapeer.ru/takelogin.php',
-			'dl_url' => 'http://megapeer.ru/download/',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<td class="heading"',
-			'redirect' => array(
-				'from' => array('http://megapeer.vip/'),
-				'to' => 'http://megapeer.ru/'
-			)
-		),
-		'tapochek' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "/https:\/\/tapochek.net\/viewtopic.php\?t=/",
-			'login_url' => 'https://tapochek.net/login.php',
-			'dl_url' => 'https://tapochek.net/download.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<p><img src="images/icon_dn.png"',
-		),
-		'uniongang' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#uniongang.club/torrent-#",
-			'login_url' => 'http://uniongang.club/takelogin.php',
-			'dl_url' => 'http://uniongang.club/download.php?id=',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<form method="post" action="takerate.php">',
-		),
-		'kinozal' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#kinozal.tv/details.php\?id=#",
-			'login_url' => 'http://kinozal.tv/takelogin.php',
-			'dl_url' => 'http://dl.kinozal.tv/download.php?id=',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<form id="cmt" method=post',
-		),
-		'kinozalguru' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#kinozal.guru/details.php\?id=#",
-			'login_url' => 'https://kinozal.guru/takelogin.php',
-			'dl_url' => 'http://dl.kinozal.guru/download.php?id=',
-			'login_input_name' => 'username',
-			'password_input_name' => 'password',
-			'target_element' => '<form id="cmt" method=post',
-		),
-		'windowssoftinfo' => array(
-			'enabled' => true,
-			'regex' => "#windows-soft.info/#",
-			'dl_url' => 'https://windows-soft.info/engine/download.php?id=',
-			'target_element' => '<div class="fstory-rating">'
-		),
-		'ztorrents' => array(
-			'enabled' => true,
-			'regex' => "#z-torrents.ru/#",
-			'dl_url' => '', // TODO
-			'target_element' => '<div class="dle_b_appp"'
-		),
-		'piratbit' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#piratbit.org/topic/#",
-			'login_url' => 'https://piratbit.org/login.php',
-			'dl_url' => 'https://piratbit.org/dl.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<span class="fs11_bold thanked">',
-		),
-		'onlysoft' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "#https://only-soft.org/viewtopic.php\?t=#",
-			'login_url' => 'https://only-soft.org/login.php',
-			'dl_url' => 'https://only-soft.org/download.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<p class="small">',
-		),
-		'rutrackerru' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "/http:\/\/rutracker.ru\/viewtopic.php\?t=/",
-			'login_url' => 'http://rutracker.ru/login.php',
-			'dl_url' => 'http://rutracker.ru/dl.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<input type="radio" name=',
-		),
-		'ddgroupclub' => array(
-			'enabled' => true,
-			'auth' => true,
-			'regex' => "/http:\/\/ddgroupclub.win\/viewtopic.php\?t=/",
-			'login_url' => 'http://ddgroupclub.win/login.php',
-			'dl_url' => 'http://ddgroupclub.win/dl.php?id=',
-			'login_input_name' => 'login_username',
-			'password_input_name' => 'login_password',
-			'target_element' => '<p class="small">',
-		),
-		'xxxtor' => array(
-			'enabled' => true,
-			'regex' => "#xxxtor.net#",
-			'dl_url' => 'https://', // TODO
-			'target_element' => '<div id="down">',
-		)
-	);
 
 	// Проверка вводимого URL адреса
 	$tracker = null;
