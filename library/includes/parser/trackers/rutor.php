4541b698e8570813bd7860b6e408e8ce67d69e2b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-31 15:59:39 +0700

    Update rutor.php

--- library/includes/parser/trackers/rutor.php
+++ library/includes/parser/trackers/rutor.php
@@ -35,21 +35,15 @@ function rutor($content, $target_element)
 	// Основные замены
 	$html = parser_base($html);
 
-	// Работа с текстом
+	// TODO Refactor
+	$html = preg_replace('/<a href="\/tag\/.*?" target="_blank">([\s\S]*?)<\/a>/', '$1', $html);
+	$html = preg_replace('/<div class="hidewrap"><div class="hidehead" onclick="hideshow.*?">([\s\S]*?)<\/div><div class="hidebody"><\/div><textarea class="hidearea">([\s\S]*?)<\/textarea><\/div>/', "[spoiler=\"\\1\"]\\2[/spoiler]", $html);
+	$html = preg_replace('/<a href="([^<]*?)" target="_blank">([^<]*?)<(?=\/)\/a>/siu', '[url=$1]$2[/url]', $html);
+	$html = preg_replace('/<img src="([^<]*?)" style="float:(.*?);" \/>/siu', '[img=$2]$1[/img]', $html);
+	$html = preg_replace('/<img src="([^<]*?)" \/>/siu', '[img]$1[/img]', $html);
 	$html = preg_replace('/<font size="([^<]*?)">([^<]*?)<(?=\/)\/font>/', "[size=2\\1]\\2[/size]", $html);
 	$html = preg_replace('/<span style="color:([^<]*?);">([^<]*?)<(?=\/)\/span>/', '[color=$1]$2[/color]', $html);
 	$html = preg_replace('/<span style="font-family:([^<]*?);">([^<]*?)<(?=\/)\/span>/', '[font="$1"]$2[/font]', $html);
-
-	// Картинки
-	$html = preg_replace('/<img src="(.*?)" (?:\s+[^>]+)*?style="(?:.*?float:(?:left|right);.*?)?" (.*?)>/siu', '[img=$2]$1[/img]', $html);
-	$html = preg_replace('/<img[^>]*?src="(.*?)"[^>]*?>/i', '[img]$1[/img]', $html);
-
-	// Ссылки
-	$html = preg_replace('/<a href="\/.*?">(.*?)<\/a>/i', '$1', $html); // Удаление ссылок, с относительными ссылками
-	$html = preg_replace('/<a href="(.*?)".*?>(.*?)<\/a>/i', '[url=$1]$2[/url]', $html);
-
-	// Спойлеры
-	$html = preg_replace('/<div class="hidewrap"><div class="hidehead" onclick="hideshow.*?">([\s\S]*?)<\/div><div class="hidebody"><\/div><textarea class="hidearea">([\s\S]*?)<\/textarea><\/div>/', "[spoiler=\"\\1\"]\\2[/spoiler]", $html);
 
 	// Вставка плеера
 	insert_video_player($html);
