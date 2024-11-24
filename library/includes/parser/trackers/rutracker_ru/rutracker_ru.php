a6d90c8fc46ee2edceafdf6fb16cd77dff111296
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-11-24 18:02:37 +0700

    Update rutracker_ru.php

--- library/includes/parser/trackers/rutracker_ru/rutracker_ru.php
+++ library/includes/parser/trackers/rutracker_ru/rutracker_ru.php
@@ -23,10 +23,8 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 function rutracker_ru($text)
 {
 	// ------------------- Get title -------------------
-	preg_match("#<h1>([\s\S]*?)</h1>#i", $text, $matches);
+	preg_match("#<title>([\s\S]*?) :: RuTracker.RU - Свободный торрент трекер</title>#", $text, $matches);
 	$title = $matches[1];
-	$title = preg_replace('/\[[\s\S]*?\.[\s\S]*? \/ [\s\S]*?\.[\s\S]*?\]/', '', $title);
-	$title = preg_replace('/\[[\s\S]*?\.[\s\S]*?\]/', '', $title);
 
 	// ------------------- Get download link -------------------
 	preg_match('#<a class="yellowBtn" href=".*?var=//(.*?)&.*?var2.*?</a>#', $text, $matches);
